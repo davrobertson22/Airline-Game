ef16f0c5982c032f78d43ae7bf1e574d0793f2ce
David Robertson <davidrobertson@Davids-MacBook-Air-167.local>
2026-06-13 13:49:05 -0700

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { GameProvider } from './store/GameContext.jsx';
 import './index.css';
+import { inject } from '@vercel/analytics';
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
