df6c43a13176401507becb8fe2d24018a52eebbf
David Robertson <davidrobertson@Davids-MacBook-Air-167.local>
2026-06-13 13:53:11 -0700

    Upgrade plugin-react for Vite 8, add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { GameProvider } from './store/GameContext.jsx';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
-import { inject } from '@vercel/analytics';
-inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GameProvider>
       <App />
     </GameProvider>
+    <Analytics />
   </React.StrictMode>
 );
